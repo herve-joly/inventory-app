33f36d815d6856d118ff5d9fe3e9cdb2a82a6084
bagnalli <stevenbagnall@outlook.com>
2023-03-09 13:36:44 +0000

    centered items

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -14,38 +14,48 @@ export const Item = (props) => {
 
   return (
     // Image View
-    <div
-      class
+    <section
       style={{
-        border: "1px solid rgb(36,38,44)",
         display: "flex",
-        flexDirection: "column",
-        padding: "10px",
-        margin: "15px",
-        borderRadius: "1.5rem",
-        width: "fit-content",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <>
-        <img
-          onClick={fetchReq}
-          class="img-viewAll"
-          src={props.item.image}
-          alt={props.item.title}
-          style={{
-            cursor: "pointer",
-            borderRadius: "2rem",
-          }}
-        />
-        <h2
-          onClick={fetchReq}
-          style={{
-            cursor: "pointer",
-          }}
-        >
-          {props.item.title}
-        </h2>
-      </>
-    </div>
+      <div
+        class
+        style={{
+          border: "1px solid rgb(36,38,44)",
+          display: "flex",
+          flexDirection: "column",
+          padding: "10px",
+          margin: "15px",
+          borderRadius: "1.5rem",
+          width: "42rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <>
+          <img
+            onClick={fetchReq}
+            class="img-viewAll"
+            src={props.item.image}
+            alt={props.item.title}
+            style={{
+              cursor: "pointer",
+              borderRadius: "2rem",
+            }}
+          />
+          <h2
+            onClick={fetchReq}
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            {props.item.title}
+          </h2>
+        </>
+      </div>
+    </section>
   );
 };
