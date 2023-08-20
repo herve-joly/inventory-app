fc4c2dd8447c938c83522002a55f0d6a138c7f2c
= <=>
2023-08-20 20:39:43 +0100

    to be deleted

--- public/react/api.js
+++ public/react/api.js
@@ -1,9 +1,12 @@
 let apiURL;
 
-if (process.env.NODE_ENV === 'development') {
-  apiURL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+if (process.env.NODE_ENV === "development") {
+  apiURL = process.env.REACT_APP_API_URL || "http://localhost:1234/api";
 } else {
-  apiURL = '/api'
+  apiURL = "/api";
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
 export default apiURL;
